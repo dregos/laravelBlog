38b83493fc70cc31b08985167ce4a056bcb62b2f
Daniel <dregojevic@gmail.com>
2017-08-16 21:33:24 +0200

    tags and post tags

--- app/Post.php
+++ app/Post.php
@@ -13,6 +13,10 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 
     public function user()
     {
